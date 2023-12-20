3dde5f8a985e73b7a9914fe1f4cc7d43502e0eac
Aziz Azizli <azizliaziz025@gmail.com>
2023-12-20 18:27:32 +0400

    create card companent and card body

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import cardimg from "../../assets/images/cardImg/cardImg.svg";
+import style from "./card.module.css";
+
+export const Card = () => {
+  return (
+    <div className={`${style.cardBottom}`}>
+      <div className={`${style.imgSize} rounded-xl`}>
+        <img src={cardimg} alt="" />
+      </div>
+      <div>
+        <p className={`${style.cardTitle}`}>hollywood</p>
+        <p className={`${style.cardDesc}`}>babbu maan</p>
+      </div>
+    </div>
+  );
+};
