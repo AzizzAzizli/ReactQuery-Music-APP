f051c7219adc1db0d8e7dfb68795a4c701d7560e
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-12-20 17:38:30 +0400

    add

--- src/components/LayoutHeader/index.jsx
+++ src/components/LayoutHeader/index.jsx
@@ -1,16 +1,15 @@
-import icon from "../../assets/icons/layoutHeaderIcon/layoutHeaderIcon.svg";
-
-export const LayoutHeader = () => {
+/* eslint-disable react/prop-types */
+export const LayoutHeader = ({ headTitle , description , icon }) => {
 
   return (
     <header className=" flex flex-col mb-6 ">
       <div className=" flex justify-between items-center">
-        <h1 className=" text-4xl font-bold text-white">Playlist</h1>
+        <h1 className=" text-4xl font-bold text-white">{headTitle}</h1>
         <div>
           <img src={icon} alt="" />
         </div>
           </div>
-          <p className=" text-xl font-normal  text-black opacity-60">304 Playlist</p>
+          <p className=" text-xl font-normal  text-black opacity-60">{description}</p>
     </header>
   );
 };
