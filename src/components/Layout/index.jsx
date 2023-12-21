b122ac47d2be9f24e78b9341f446016b1c8177e1
Aziz Azizli <azizliaziz025@gmail.com>
2023-12-21 16:26:36 +0400

    create search function  fix some js bugs

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,113 +1,171 @@
 /* eslint-disable react/jsx-key */
 import layoutHeaderIcon from "../../assets/icons/layoutHeaderIcon/layoutHeaderIcon.svg";
-import { getQuickMusic } from '../../services/index'
+import { getQuickMusic, searchMusic } from "../../services/index";
 import { LayoutHeader } from "../LayoutHeader/index";
 import { LayoutInput } from "../LayoutInput/index";
 import { Card } from "../Card";
-import { useQuery, } from "react-query";
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { useEffect, useRef, useState } from "react";
 const Layout = () => {
-const [musicName,setMusicName]=useState("")
-    const {
-      data: quickMusics,
-      isLoading,
-      isError,
-      error,
-      refetch
-    } = useQuery({
-      queryKey: "quickMusic",
-      queryFn: async () => {
-        const response = await getQuickMusic();
-        return response.data.results
-      },
-      onError: (err) => {
-        console.log("err", err);
-      },
-      refetchInterval: false,
-      refetchIntervalInBackground: false,
-      refetchOnWindowFocus: false,
-    });
-  
-    // const {
-    //   data: musicDatas,
-    //   isLoading,
-    //   isError,
-    //   error,
-    //   refetch
-    // } = useQuery({
-    //   queryKey: "musicDatas",
-    //   queryFn: async () => {
-    //     const response = await ()=>searchMusic();
-    //     return response.data.results
-    //   },
-    //   onError: (err) => {
-    //     console.log("err", err);
-    //   },
-    //   refetchInterval: false,
-    //   refetchIntervalInBackground: false,
-    //   refetchOnWindowFocus: false,
-    // });
+  const [musicName, setMusicName] = useState("");
 
+  let inputRef = useRef();
 
-function handleEnter(e) {
-  // console.log(e.key);
-  if (e.key === "Enter") {
-    console.log((musicName));
-    setMusicName("")
+  const {
+    data: quickMusics,
+    isLoading,
+    isError,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: "quickMusic",
+    queryFn: async () => {
+      const response = await getQuickMusic();
+      return response.data.results;
+    },
+    onError: (err) => {
+      console.log("err", err);
+    },
+    refetchInterval: false,
+    refetchIntervalInBackground: false,
+    refetchOnWindowFocus: false,
+  });
+
+  const {
+    data: musicDatas,
+    isLoading: isLoadingSearch,
+    isError: isErrorSearch,
+  } = useQuery({
+    queryKey: "musicDatas",
+    queryFn: async () => {
+      const response = await searchMusic(musicName);
+
+      return response;
+    },
+    onError: (err) => {
+      console.log("err", err);
+    },
+  });
+
+  console.log(musicDatas, isErrorSearch);
+
+  function handleEnter(e) {
+    let value = inputRef.current.value;
+
+    if (e.key === "Enter") {
+      // console.log(inputRef.current.value);
+
+      setMusicName(value);
+      inputRef.current.value = "";
+    }
   }
-}
-  function handleInputValue(e) {
-    // console.log(e.target.value);
-    let value=e.target.value;
-    setMusicName(value)
-}
-    return (
-      <div className="layout w-[70%] h-full p-7">
-        <LayoutHeader
-          
-          headTitle={"Playlist"}
-          description={`${quickMusics?quickMusics?.length:"..."} Playlist`}
-          icon={layoutHeaderIcon}
-        />
-        <LayoutInput value={musicName} onKeyDown={handleEnter} onChange={handleInputValue } />
 
-        {
-          isLoading ? 
-              <div aria-label="Loading..." role="status" className="flex justify-center w-full items-center mt-20 space-x-2">
-                <svg className="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
-                    <line x1="128" y1="32" x2="128" y2="64" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
-                    <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" strokeLinecap="round" strokeLinejoin="round"
-                        strokeWidth="24"></line>
-                    <line x1="224" y1="128" x2="192" y2="128" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24">
-                    </line>
-                    <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" strokeLinecap="round" strokeLinejoin="round"
-                        strokeWidth="24"></line>
-                    <line x1="128" y1="224" x2="128" y2="192" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24">
-                    </line>
-                    <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" strokeLinecap="round" strokeLinejoin="round"
-                        strokeWidth="24"></line>
-                    <line x1="32" y1="128" x2="64" y2="128" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"></line>
-                    <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24">
-                    </line>
-                </svg>
-                <span className="text-4xl font-medium text-gray-500">Loading...</span>
-              </div>
-          : ""
-        }
+  console.log(musicName);
+  return (
+    <div className="layout w-[70%] h-full p-7">
+      <LayoutHeader
+        headTitle={"Playlist"}
+        description={`${quickMusics ? quickMusics?.length : "..."} Playlist`}
+        icon={layoutHeaderIcon}
+      />
+      <LayoutInput ref={inputRef} onKeyDown={handleEnter} />
 
-        <section className={` gap-[23.04px] cardBody`}>
-            {
-              quickMusics?.map((musicItem) => (
-                  <Card
-                      {...musicItem}
-                      key={musicItem.videoId} 
-                  />
-              ))
-            }
-          
-        </section>
-      </div>
-    );
+      {isLoading ? (
+        <div
+          aria-label="Loading..."
+          role="status"
+          className="flex justify-center w-full items-center mt-20 space-x-2"
+        >
+          <svg
+            className="h-20 w-20 animate-spin stroke-gray-500"
+            viewBox="0 0 256 256"
+          >
+            <line
+              x1="128"
+              y1="32"
+              x2="128"
+              y2="64"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="24"
+            ></line>
+            <line
+              x1="195.9"
+              y1="60.1"
+              x2="173.3"
+              y2="82.7"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="24"
+            ></line>
+            <line
+              x1="224"
+              y1="128"
+              x2="192"
+              y2="128"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="24"
+            ></line>
+            <line
+              x1="195.9"
+              y1="195.9"
+              x2="173.3"
+              y2="173.3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="24"
+            ></line>
+            <line
+              x1="128"
+              y1="224"
+              x2="128"
+              y2="192"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="24"
+            ></line>
+            <line
+              x1="60.1"
+              y1="195.9"
+              x2="82.7"
+              y2="173.3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="24"
+            ></line>
+            <line
+              x1="32"
+              y1="128"
+              x2="64"
+              y2="128"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="24"
+            ></line>
+            <line
+              x1="60.1"
+              y1="60.1"
+              x2="82.7"
+              y2="82.7"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="24"
+            ></line>
+          </svg>
+          <span className="text-4xl font-medium text-gray-500">Loading...</span>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <section className={` gap-[23.04px] cardBody`}>
+        {quickMusics?.map((musicItem) => (
+          <Card {...musicItem} key={musicItem.videoId} />
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Layout;
--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,21 +1,33 @@
-import {baseUrl} from '../constants/base/baseUrl'
+import { baseUrl } from "../constants/base/baseUrl";
 import axios from "axios";
 
 const instanceAxios = axios.create({
-    baseURL: baseUrl,
-    params: {gl: 'ID'},
-    headers: {
-        'X-RapidAPI-Key': '9076eac4a5msha9000f7a84a1cb1p112ed6jsn13542991e11b',
-        'X-RapidAPI-Host': 'youtube-music-api3.p.rapidapi.com'
-    }
+  baseURL: baseUrl,
+  params: { gl: "ID" },
+  headers: {
+    "X-RapidAPI-Key": "9076eac4a5msha9000f7a84a1cb1p112ed6jsn13542991e11b",
+    "X-RapidAPI-Host": "youtube-music-api3.p.rapidapi.com",
+  },
 });
 
-
 export const getQuickMusic = async () => {
-    const response = await instanceAxios.get(`recommend`);
-    return response;
+  const response = await instanceAxios.get(`recommend`);
+  return response;
 };
 
-// export const searchMusic = async (value) => {
-//     const respons =await instanceAxios.get(`search`, value);
-// }
+
+
+export const searchMusic = async (value) => {
+  const response = await instanceAxios.get(`search`, {
+    params: {
+      q: value,
+      type: "song",
+    },
+    headers: {
+      "X-RapidAPI-Key": "f54eea0568msh2bd264fabc3a92bp19a66ajsnbc96b4f8fbe7",
+      "X-RapidAPI-Host": "youtube-music-api3.p.rapidapi.com",
+    },
+  });
+
+  return response;
+};
