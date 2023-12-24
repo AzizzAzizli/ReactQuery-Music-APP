1fd6cc277a831865a66b74ae036a48fc7cf72507
Aziz Azizli <azizliaziz025@gmail.com>
2023-12-24 22:47:42 +0400

    add active link border for side bar links

--- src/components/SidebarItem/SidebarItem.jsx
+++ src/components/SidebarItem/SidebarItem.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom"
 
 /* eslint-disable react/prop-types */
-const SidebarItem = ({ title , icon , route }) => {
+const SidebarItem = ({ title , icon , route , activeLink}) => {
   const navigate = useNavigate()
   return (
     <div onClick={() => navigate(route)} className="flex gap-6 items-center cursor-pointer">
@@ -9,7 +9,7 @@ const SidebarItem = ({ title , icon , route }) => {
             alt="" 
             src={icon} 
         />
-        <p className="text-white text-xl">
+      <p className={`text-white text-xl ${activeLink?"  border-b-2  border-white-800":""}`}>
             {
                 title
             }
